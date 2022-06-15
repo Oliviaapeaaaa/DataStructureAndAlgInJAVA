7a58e580b6012cce9555612a2bf39ddf7705cd68
olivia chen <99147257+Oliviaapeaaaa@users.noreply.github.com>
2022-06-15 14:41:15 -0400

    SingleLinkedList Implement
    1. remove
    2. add

--- DataStrctureInJava/src/linkedlist/LLDemo.java
+++ DataStrctureInJava/src/linkedlist/LLDemo.java
@@ -1,24 +1,42 @@
 package linkedlist;
 
 import java.util.Deque;
-
 public class LLDemo {
     public static void main(String[] args) {
-        Deque<String> list = new SingleLList<>();
-         for (String S : new String[] {"A", "BB","CCC","DDDD"}){
-             list.addFirst(S);
-         }
-         System.out.println(list);
+   Deque<String> L = new SingleLList<>();
+//    Deque<String> L = new DoubleLList<>();
 
-//         list.addLast("ZZZZ");
-//         System.out.println(list);
-////        ((SingleLList<Integer>) list).search(0);
-//        list.removeFirst();
-//        System.out.println(list);
+        for (String str : new String[]{"aaa", "bbb", "ccc", "ddd", "zzz"}) {
+            L.addFirst(str);
+        }
+        System.out.println(L);
+         for (String s : L){
+             System.out.println( s + "" );
+         }
+         System.out.println();
+         System.out.println(L.getFirst());
+         System.out.println();
+
+         System.out.println(((SingleLList<String>) L).search("ddd"));
+        System.out.println();
+        System.out.println(L.removeFirst());
+        System.out.println("After remove first node : " + L);
+
+        L.addLast("s");
+        System.out.println(L);
+
+        ((SingleLList<String>) L).add(1,"iii");
+        System.out.println("after insert at 1 : " + L);
+        System.out.println("remove last : " + L.removeLast());
+        System.out.println(L);
+
+        System.out.println();
+        ((SingleLList<String>) L).remove("iii");
+        System.out.println("after reomve \"iii\" "  + L);
+
+        ((SingleLList<String>) L).remove(2);
+        System.out.println("after reomve index at 2 "  + L);
+        System.out.println(L.size());
 
     }
-
-
-
-
 }
--- DataStrctureInJava/src/linkedlist/SingleLList.java
+++ DataStrctureInJava/src/linkedlist/SingleLList.java
@@ -1,25 +1,21 @@
 package linkedlist;
 
-import org.w3c.dom.Node;
-
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 public class SingleLList<E> extends LListAdapter<E>{
 
     private static final long serialVersionUID = 1l;
 
 
-    private static class Node<E>{
+    private  class Node{
        E value;
-       Node<E> next;
-       Node(E value, Node<E> next) {
+       Node next;
+       Node(E value, Node next) {
            this.value  = value;
            this.next = next;
        }
-
-
-
     }
-    private Node<E> head;
+    private Node head;
     private int size;
 
     public SingleLList(){
@@ -33,44 +29,71 @@ public class SingleLList<E> extends LListAdapter<E>{
         return head == null;
     }
 
+    public int size(){
+        return this.size;
+    }
+
     @Override
     public void clear(){ head = null;}
 
-    public Node getHead() {
-        return head;
+    public E getFirst() {
+        return head.value;
     }
     @Override
     public void addFirst(E value){
-        Node newNode = new Node<E>(value,head);
+        Node newNode = new Node(value,head);
         newNode.next = head;
         newNode.value = value;
         head = newNode;
         size ++;
     }
 
+    @Override
     public void addLast(E value){
-        if(isEmpty()){
+        if (isEmpty()){
             addFirst(value);
+            return;
         }
 
-        Node newNode = new Node<>(value,null);
-        newNode.value = value;
-        newNode.next = null;
-        Node last = head;
+        Node newNode = new Node(value,null);
+//        newNode.next = null;
+//        newNode.value = value;
+
+
+        Node last = this.head;
         while (last.next != null){
-            last = head.next;
+            last = last.next;
         }
         last.next = newNode;
+
         size ++;
     }
 
+    @Override
     public boolean add(E value){
         addLast(value);
         return true;
     }
 
+   @Override
+    public void add(int index, E value){
+        if (index < 0 || index > size){
+            throw
+                    new IndexOutOfBoundsException("index " + index + " size " + size);
+        }
 
+        Node newNode = new Node(value, head);
+        Node cur = head;
+//        locate the node at index position by crawling from left to right
+        for (int counter = 0; counter < index - 1; counter ++){
+            cur = cur.next;
+        }
+        Node temp = cur.next;
+        cur.next = newNode;
+        newNode.next = temp;
+        size ++;
 
+    }
     @Override
     public E removeFirst(){
         if (isEmpty()){
@@ -85,16 +108,126 @@ public class SingleLList<E> extends LListAdapter<E>{
         return saved;
     }
 
-//   public boolean search(Object obj){
-//        Node curNode  = this.head;
-//        while (curNode.next != null){
-//            if (curNode.value.equals(obj)){
-//                return true;
-//            }
-//            curNode = curNode.next;
+    @Override
+    public E removeLast(){
+         if (isEmpty()){
+             throw
+                     new NoSuchElementException();
+         }
+
+         Node cur = head;
+        for (int counter = 0; counter < size - 2; counter ++){
+            cur = cur.next;
+        }
+        E data = cur.next.value;
+        cur.next = null;
+        size --;
+        return data;
+    }
+
+    @Override
+    public boolean remove(Object obj){
+        if (isEmpty()){
+            throw
+                    new NoSuchElementException();
+        }
+        Node cur = head;
+        Node pre = null;
+        while (cur.next != null){
+            if (cur.value.equals(obj)){
+                pre.next = cur.next;
+                size --;
+                return true;
+            }
+            pre = cur;
+            cur = cur.next;
+        }
+        return false;
+
+    }
+    @Override
+    public E remove(int index){
+
+        if (isEmpty()){
+            throw
+                    new NoSuchElementException();
+        }
+        Node pre = null;
+        Node cur = head;
+
+        for (int counter = 0; counter < index; counter ++){
+            pre = cur;
+            cur = cur.next;
+        }
+        E data = cur.value;
+        pre.next = cur.next;
+        size --;
+        return data;
+    }
+
+    public boolean search(Object obj){
+        Node curNode  = this.head;
+        while (curNode.next != null){
+            if (curNode.value.equals(obj)){
+                return true;
+            }
+            curNode = curNode.next;
+        }
+        return false;
+   }
+
+    @Override
+    public String toString() {
+        Iterator<E> iter = iterator();
+        int i = 0;
+        String str = "[";
+        while(iter.hasNext()) {
+            if (i++ != 0) {
+                str += ", ";
+            }
+            str += iter.next();
+        }
+        str += "]";
+        return str;
+    }
+
+    @Override
+    public Iterator<E> iterator(){
+        return new Iterator<E>() {
+            private SingleLList.Node cur = head;
+            @Override
+            public boolean hasNext() {
+                return cur!= null;
+            }
+
+            @Override
+            public E next() {
+                E data = (E) cur.value;
+                cur = cur.next;
+                return data;
+            }
+        };
+    }
+//   public void printList() {
+//        if (isEmpty()) {
+//            System.out.println("List is Empty!");
+//            return;
 //        }
-//        return false;
-//   }
+//
+//        Node temp = headNode;
+//        System.out.print("List : ");
+//
+//        while (temp.nextNode != null) {
+//            System.out.print(temp.data.toString() + " -> ");
+//            temp = temp.nextNode;
+//        }
+//
+//        System.out.println(temp.data.toString() + " -> null");
+//    }
+
+
+
+
 
 
 
